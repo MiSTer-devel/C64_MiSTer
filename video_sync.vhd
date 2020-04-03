98b905215c1f700bacee6054f21b8eda18330342
sorgelig <pour.garbage@gmail.com>
2020-04-04 02:44:00 +0800

    Fix NTSC mode.

--- video_sync.vhd
+++ video_sync.vhd
@@ -37,6 +37,7 @@ begin
 process(clk32)
 	variable  dot_count : integer range 0 to 1023 := 0;
 	variable line_count : integer range 0 to 511 := 0;
+	variable line_reset : std_logic := '0';
 	begin
 	if falling_edge(clk32) then
 		hsync_r0 <= hsync;
@@ -54,30 +55,35 @@ process(clk32)
 
 			if hsync_r = '0' and hsync = '1' then
 				dot_count := 0;
-				line_count := line_count + 1;
+				if line_reset = '1' then
+					line_count := 0;
+					line_reset := '0';
+				else
+					line_count := line_count + 1;
+				end if;
 			else
 				dot_count := dot_count + 1;
 			end if;
 
 			if vsync_r = '0' and vsync = '1' then
-				line_count := 0;
+				line_reset := '1';
 			end if;
 			
 			if ntsc = '1' then
-				if dot_count     = 010 then hsync_out <= '1';
+				if dot_count     = 016 then hsync_out <= '1';
 					if line_count = 000 then vsync_out <= '1'; end if;
 					if line_count = 004 then vsync_out <= '0'; end if;
 				end if;
-				if dot_count     = 048 then hsync_out <= '0'; end if;
+				if dot_count     = 054 then hsync_out <= '0'; end if;
 
 				if wide = '0' then
-					if dot_count  = 510 then hblank <= '1'; end if;
-					if dot_count  = 096 then hblank <= '0'; end if;
+					if dot_count  = 516 then hblank <= '1'; end if;
+					if dot_count  = 112 then hblank <= '0'; end if;
 					if line_count = 000 then vblank <= '1'; end if;
 					if line_count = 012 then vblank <= '0'; end if;
 				else
-					if dot_count  = 510 then hblank <= '1'; end if;
-					if dot_count  = 096 then hblank <= '0'; end if;
+					if dot_count  = 516 then hblank <= '1'; end if;
+					if dot_count  = 112 then hblank <= '0'; end if;
 					if line_count = 242 then vblank <= '1'; end if;
 					if line_count = 035 then vblank <= '0'; end if;
 				end if;
