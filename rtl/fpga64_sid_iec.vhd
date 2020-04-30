080cfcbde2425db8e0c7785ca9726745f4bc4af5
sorgelig <pour.garbage@gmail.com>
2020-04-30 20:33:56 +0800

    Simulate a glitch in the C64C PLA.

--- rtl/fpga64_sid_iec.vhd
+++ rtl/fpga64_sid_iec.vhd
@@ -220,6 +220,7 @@ signal vicDiAec     : unsigned(7 downto 0);
 signal vicAddr      : unsigned(15 downto 0);
 signal vicData      : unsigned(7 downto 0);
 signal lastVicDi    : unsigned(7 downto 0);
+signal vicAddr1514  : unsigned(1 downto 0);
 signal colorQ       : unsigned(3 downto 0);
 signal colorData    : unsigned(3 downto 0);
 signal colorDataAec : unsigned(3 downto 0);
@@ -833,8 +834,23 @@ uart_dcd_out <= cia2_pbo(4);
 -- -----------------------------------------------------------------------
 -- VIC bank to address lines
 -- -----------------------------------------------------------------------
-vicAddr(14) <= (not cia2_pao(0));
-vicAddr(15) <= (not cia2_pao(1));
+-- The glue logic on a C64C will generate a glitch during 10 <-> 01
+-- generating 00 (in other words, bank 3) for one cycle.
+--
+-- When using the data direction register to change a single bit 0->1
+-- (in other words, decreasing the video bank number by 1 or 2),
+-- the bank change is delayed by one cycle. This effect is unstable.
+process(clk32)
+begin
+	if rising_edge(clk32) then
+		if phi0_cpu = '0' and enableVic = '1' then
+			vicAddr1514 <= not cia2_pao(1 downto 0);
+		end if;
+	end if;
+end process;
+
+-- emulate only the first glitch (enough for Undead from Emulamer)
+vicAddr(15 downto 14) <= "11" when ((vicAddr1514 xor not cia2_pao(1 downto 0)) = "11") and (cia2_pao(0) /= cia2_pao(1)) else not unsigned(cia2_pao(1 downto 0));
 
 -- -----------------------------------------------------------------------
 -- Interrupt lines
