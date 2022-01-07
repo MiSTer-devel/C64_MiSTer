5de5507003c60b3831b56f195e6e0d30d12254a6
Andrea Bogazzi <andreabogazzi79@gmail.com>
2022-01-07 13:03:55 +0100

    Allow for 1440p vertical crop (#123)

--- c64.sv
+++ c64.sv
@@ -1228,6 +1228,12 @@ always @(posedge CLK_VIDEO) begin
 		if(HDMI_HEIGHT == 1080) vcrop <= 10'd216;
 		if(HDMI_HEIGHT == 1200) vcrop <= 240;
 	end
+	else if(HDMI_WIDTH >= 1440 && !scandoubler) begin
+		// 1920x1440 and 2048x1536 are 4:3 resolutions and won't fit in the previous if statement ( width > height * 1.5 )
+		if(HDMI_HEIGHT == 1440) vcrop <= 240;
+		// 2 extra lines compared to 1080 cropping
+		if(HDMI_HEIGHT == 1536) vcrop <= 218;
+	end
 end
 
 
