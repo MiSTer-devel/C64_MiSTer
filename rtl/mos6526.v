d14a5b4665bc49ebf3f70df2f21614a65e0f2c2e
Dan <dan@wadoku.de>
2022-11-04 06:18:46 +0100

    CIA: fix timer reset values (Arctic Shipwreck) (gyurco)
    ported from https://github.com/mist-devel/c64/commit/70b8d93da119c0b08e444adfacf4b68905e318bb
    fixes #107

--- rtl/mos6526.v
+++ rtl/mos6526.v
@@ -195,7 +195,7 @@ always @(posedge clk) begin
     ta_lo          <= 8'hff;
     ta_hi          <= 8'hff;
     cra            <= 8'h00;
-    timer_a        <= 16'h0000;
+    timer_a        <= 16'hffff;
     timerAff       <= 1'b0;
     icr[0]         <= 1'b0;
   end
@@ -265,7 +265,7 @@ always @(posedge clk) begin
     tb_lo          <= 8'hff;
     tb_hi          <= 8'hff;
     crb            <= 8'h00;
-    timer_b        <= 16'h0000;
+    timer_b        <= 16'hffff;
     timerBff       <= 1'b0;
     icr[1]         <= 1'b0;
     timer_b_int    <= 0;
