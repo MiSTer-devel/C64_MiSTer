e863ef121277bb3ed301e71835cfc43067ad85f0
Michael Jørgensen <michael.finn.jorgensen@gmail.com>
2023-04-28 18:47:45 +0200

    Add support for Ocean type 2 (#151)
    * Add support for Ocean Type B
    
    (cherry picked from commit 510fc8039da40f8b69af18522ce87be3dccb8365)
    
    * Fix support for Ocean Type B
    
    (cherry picked from commit 958ae1447fd00f7e0b7b6e93d524245759a0fa73)
    
    ---------
    
    Co-authored-by: Michael Finn Jørgensen <mfj@weibel.dk>

--- rtl/cartridge.v
+++ rtl/cartridge.v
@@ -272,11 +272,20 @@ always @(posedge clk32) begin
 		// BANK is written to lower 6 bits of $DE00 - bit 8 is always set
 		// best to mirror banks at $8000 and $A000
 		5:	begin
-				exrom_overide <= 0;
-				game_overide  <= 0;
+				if(!init_n) begin
+					exrom_overide <= 0;
+					game_overide  <= 0;
+				end
 				if(ioe_wr) begin
 					bank_lo <= data_in[5:0];
 					bank_hi <= data_in[5:0];
+				end
+				// Autodetect Ocean Type B (512k)
+				// Only $8000 is used, while $A000 is RAM
+				if(cart_bank_wr) begin
+					if(cart_bank_num>=32) begin
+						game_overide <= 1;
+					end
 				end
 			end
 
